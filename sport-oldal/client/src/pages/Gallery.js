4df4438cd4fb169ed47ebd485acb623ed864dc3f
S.György <schmidt.szie@gmail.com>
2023-05-02 19:50:37 +0200

    add new element gallery :)

--- sport-oldal/client/src/pages/Gallery.js
+++ sport-oldal/client/src/pages/Gallery.js
@@ -5,6 +5,9 @@ export default function Gallery() {
         <div>
             <h1>Galéria</h1>
             <h2>Második sor</h2>
+            <iframe className='m-4' width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" 
+            title="YouTube video player" frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
     )
 }
