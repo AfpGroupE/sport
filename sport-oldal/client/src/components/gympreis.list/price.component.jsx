360dae40913fc512f20dff7a883d6ceb3a0feda4
S.György <schmidt.szie@gmail.com>
2023-05-03 18:25:25 +0200

    price list menu formatting

--- sport-oldal/client/src/components/gympreis.list/price.component.jsx
+++ sport-oldal/client/src/components/gympreis.list/price.component.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 
 const GymPreis = (props) => {
-    const {textFirst, textSecond, preisHuf} = props;
-    return(
-        <div  className=''>
-             <div>
-                <h2>{textFirst}</h2>
-                <p>{textSecond}</p>
-                <p>Ár: {preisHuf} HUF</p>
-             </div>
+    const { textFirst, textSecond, preisHuf } = props;
+    return (
+        <div className='text-xl text-blue-100 m-3 text font-mono font-bold bg-white  bg-opacity-20 backdrop-blur-lg rounded drop-shadow-lg'>
+            <div className="p-2 m-2 flex-none">
+                <h2 className="flex">{textFirst}</h2>
+                <div className="flex justify-between">
+                    <p className="">{textSecond}</p>
+                    <p className="">Ár: {preisHuf} HUF</p>
+                </div>
+
+            </div>
         </div>
     )
 
--- sport-oldal/client/src/components/gympreis.list/pricelistText.js
+++ sport-oldal/client/src/components/gympreis.list/pricelistText.js
@@ -1,52 +1,52 @@
 export const models = [
     {
       textFirst: "1 alkalom",
-      textSecond: "Konditerem 0-24 (1 napig érvényes)",
+      textSecond: "Konditerem 0-24h (1 napig érvényes)",
       preisHuf: 2000,
     },
     {
       textFirst: "1 alkalmas diák / nyugdíjas",
-      textSecond: "Konditerem 0-24 (1 napig érvényes)",
+      textSecond: "Konditerem 0-24h (1 napig érvényes)",
       preisHuf: 1000,
     },
     {
       textFirst: "5 alkalmas bérlet",
-      textSecond: "Konditerem 0-24 (2 hónapig érvényes)",
+      textSecond: "Konditerem 0-24h (2 hónapig érvényes)",
       preisHuf: 7000,
     },
     {
       textFirst: "5 alkalmas diák / nyugdíjas bérlet",
-      textSecond: "Konditerem 0-24 (2 hónapig érvényes)",
+      textSecond: "Konditerem 0-24h (2 hónapig érvényes)",
       preisHuf: 3000,
     },
     {
       textFirst: "10 alkalmas bérlet",
-      textSecond: "Konditerem 0-24 (2 hónapig érvényes)",
+      textSecond: "Konditerem 0-24h (2 hónapig érvényes)",
       preisHuf: 10000,
     },
     {
       textFirst: "10 alkalmas diák / nyugdíjas bérlet",
-      textSecond: "Konditerem 0-24 (2 hónapig érvényes)",
+      textSecond: "Konditerem 0-24h (2 hónapig érvényes)",
       preisHuf: 5000,
     },
     {
       textFirst: "Havi korlátlan bérlet",
-      textSecond: "Konditerem 0-24 (1 hónapig érvényes)",
+      textSecond: "Konditerem 0-24h (1 hónapig érvényes)",
       preisHuf: 20000,
     },
     {
       textFirst: "Havi korlátlan diák / nyugdíjas bérlet",
-      textSecond: "Konditerem 0-24 (1 hónapig érvényes)",
+      textSecond: "Konditerem 0-24h (1 hónapig érvényes)",
       preisHuf: 10000,
     },
     {
       textFirst: "6 havi korlátlan bérlett",
-      textSecond: "Konditerem 0-24 (6 hónapig érvényes)",
+      textSecond: "Konditerem 0-24h (6 hónapig érvényes)",
       preisHuf: 100000,
     },
     {
       textFirst: "Éves korlátlan bérlet",
-      textSecond: "Konditerem 0-24 (12 hónapig érvényes)",
+      textSecond: "Konditerem 0-24h (12 hónapig érvényes)",
       preisHuf: 150000,
     },
   ];
