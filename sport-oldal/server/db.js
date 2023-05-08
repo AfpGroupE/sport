e342632d8cdfeb9271e9c22396bfb4ef0d0fad07
ocbtomi36 <ocbtomi36@outlook.hu>
2023-05-08 12:04:46 +0200

    database root set globaly in .env file

--- sport-oldal/server/db.js
+++ sport-oldal/server/db.js
@@ -3,10 +3,10 @@ require('dotenv').config()
 
 const pool = new Pool({
     
-    user: 'postgres',
-    password: 'Server',
-    host: 'localhost',
-    port: 5432,
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    host: process.env.HOST,
+    port: process.env.DBPORT,
     database: 'applikacio'
  }
 )
