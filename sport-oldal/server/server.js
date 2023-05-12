20ec7cccd8b0cd1928f45901d6eda0d8075eb354
ocbtomi36 <ocbtomi36@outlook.hu>
2023-05-12 19:47:41 +0200

    .env username modified and app.post created

--- sport-oldal/server/server.js
+++ sport-oldal/server/server.js
@@ -15,15 +15,30 @@ app.get("/", (req, res) => {
 
 // get all adatbázis dolgok
 // új felhasználó hozzáadaása az adatbázishoz
-/* app.post("/adduser", async (req, res) => {
+app.post("/adduser", async (req, res) => {
+    const userSzemSzam = 'aléskdfjasd';
+    const user_Nev = 'Ocb';
+    const user_szul_ido = '1987.01.01';
+    const user_felhasznalo_nev = 'Tomika';
+    const jelszo = 'hali';
+    const e_mail = 't@t.com';
+    const reg_datum = '1234.01.01';
+    const user_role = 3;
 try {
-    console.log(req.body);
+    //const {ujfelhasznalo} = req.body;
+    
+    const newUser = await pool.query(
+        "INSERT INTO users (user_Szem_Szam ,user_Nev ,user_szul_ido ,user_felhasznalo_nev ,jelszo ,e_mail ,reg_datum ,user_role ) VALUES ($1,$2,$3,$4,$5,$6,$7,$8)" , [userSzemSzam,user_Nev,user_szul_ido,user_felhasznalo_nev,jelszo,e_mail,reg_datum,user_role]
+    )
+
+    res.json(newUser);
 } catch (err) {
     console.log(err.message)
 }
 })
-*/
+
 app.get('/allusers' , async (req, res) => {
+    console.log(process.env)
     try {
         const felhasznalok = await pool.query('SELECT * FROM users;');
         res.json(felhasznalok.rows)
