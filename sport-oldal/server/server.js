fa66b757d5366c314dd8eeb222518276776c5327
ocbtomi36 <ocbtomi36@outlook.hu>
2023-05-14 01:02:03 +0200

    morgan extencion added and finished userapi

--- sport-oldal/server/server.js
+++ sport-oldal/server/server.js
@@ -2,49 +2,118 @@ const PORT =5000
 
 const express = require('express')
 const app = express()
+const morgan =require("morgan");
 const cors = require("cors");
 const pool = require('./db')
 
 app.use(cors());
+
 app.use(express.json());
+
+app.use(express.json())
 
 app.get("/", (req, res) => {
   res.send("Hello Afp 2-es csoport");
   
 })
+//visszaadja az összes usert aki az adatbázisban van
 
-// get all adatbázis dolgok
-// új felhasználó hozzáadaása az adatbázishoz
-app.post("/adduser", async (req, res) => {
-    const userSzemSzam = 'aléskdfjasd';
-    const user_Nev = 'Ocb';
-    const user_szul_ido = '1987.01.01';
-    const user_felhasznalo_nev = 'Tomika';
-    const jelszo = 'hali';
-    const e_mail = 't@t.com';
-    const reg_datum = '1234.01.01';
-    const user_role = 3;
-try {
-    //const {ujfelhasznalo} = req.body;
+app.get("/api/v1/getalluser", async (req,res) => {
+    try {
+        const result = await pool.query("SELECT * FROM users;")
+        console.log(result);
+        res.status(200).json({
+        status: "siker",
+        result: result.rows.length,
+        data: {
+            users : result.rows,
+        }
+        
+    })
+    } catch (err) {
+        console.log(err);
+    }
     
-    const newUser = await pool.query(
-        "INSERT INTO users (user_Szem_Szam ,user_Nev ,user_szul_ido ,user_felhasznalo_nev ,jelszo ,e_mail ,reg_datum ,user_role ) VALUES ($1,$2,$3,$4,$5,$6,$7,$8)" , [userSzemSzam,user_Nev,user_szul_ido,user_felhasznalo_nev,jelszo,e_mail,reg_datum,user_role]
-    )
+})
 
-    res.json(newUser);
+//visszaad egy usert
+app.get("/api/v1/user/:id", async (req,res) => {
+    console.log(req.params.id);
+    try {
+        const result = await pool.query(
+            "SELECT * FROM users where iduser = $1 ", [req.params.id]);
+
+            res.status(200).json({
+                status: "siker",
+                data: {
+                    user: result.rows[0],
+                },
+            });
+    } catch (err) {
+        console.log(err.message);
+    }
+    
+})
+
+// új felhasználó hozzáadaása az adatbázishoz
+
+app.post("/api/v1/adduser", async (req, res) => {
+
+    console.log(req.body);
+    
+try {
+    const newUser = await pool.query(
+        "INSERT INTO users (user_Szem_Szam ,user_Nev ,user_szul_ido ,user_felhasznalo_nev ,jelszo ,e_mail ,reg_datum ,user_role ) VALUES ($1,$2,$3,$4,$5,$6,$7,$8) returning *" , [req.body.userSzemSzam, 
+            req.body.user_Nev, req.body.user_szul_ido,
+            req.user_felhasznalo_nev,req.body.jelszo,
+            req.body.e_mail,req.body.reg_datum,
+            req.body.user_role]);
+    console.log(newUser)
+    res.status(201).json({
+        status: "siker",
+        data: {
+            user: newUser.rows[0],
+        },
+    });
+    
 } catch (err) {
     console.log(err.message)
 }
 })
 
-app.get('/allusers' , async (req, res) => {
-    console.log(process.env)
+// update user
+
+app.put("/api/v1/users/:id", async (req,res) => {
     try {
-        const felhasznalok = await pool.query('SELECT * FROM users;');
-        res.json(felhasznalok.rows)
+        const update = await pool.query(
+            "UPDATE users SET user_Szem_Szam = $1,user_Nev = $2,user_szul_ido = $3,user_felhasznalo_nev = $4,jelszo = $5,e_mail = $6,reg_datum = $7,user_role = $8 where id = $10 returning *",
+            [req.body.user_Nev, req.body.user_szul_ido,
+                req.user_felhasznalo_nev,req.body.jelszo,
+                req.body.e_mail,req.body.reg_datum,
+                req.body.user_role,req.body.id]
+        );
+        res.status(200).json({
+                status: "siker",
+                data: {
+                    user: update.rows[0],
+                },
+            });
     } catch (err) {
-        console.error(err)
+        console.log(err.message)
     }
+    
+});
+
+app.delete("/api/v1/users/:id", async (req,res) => {
+    try {
+        const result = await pool.query("DELETE FROM users where iduser = $1 ", [req.params.id]);
+        res.status(204).json({
+            status:"siker"
+        })
+    } catch (err) {
+        console.log(err.message)
+    }
+    
 })
 
 app.listen(PORT, ()=> console.log( `server started on port ${PORT}`))
