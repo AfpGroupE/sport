fb78e22e10e16bde5df3f99b0d51740185d08e97
vasadam1 <118050048+vasadam1@users.noreply.github.com>
2023-06-14 13:47:33 +0200

    News.js tartalommal bővítve (2 új hír)
    News.js tartalommal bővítve (2 új hír)

--- sport-oldal/client/src/pages/News.js
+++ sport-oldal/client/src/pages/News.js
@@ -2,8 +2,31 @@ import React from 'react'
 
 export default function News() {
     return (
-        <div>
-            <h1 className="text-center font-mono font-extrabold m-5 text-5xl text-blue-100">Aktuális hírek</h1>
-        </div>
+        <div className="flex flex-col items-center justify-center w-screen h-screen mx-auto">
+            <h1 className="text-center font-mono font-extrabold m-5 text-5xl text-blue-100">Aktuális híreink</h1>
+            <div><p>a</p></div>
+            <div className="flex flex-row justify-center flex-1">
+        <p className="w-1/2 mr-2 text-white items-center"><h1>2023.06.10 - Megnyílt kávézó helységünk</h1><br/>Egy csésze jó kávéval vagy teával minden könnyebben megy - még az edzés is. Ahogy edzőtermünk, úgy az általunk kínált kávé is professzionális. Mind ízben, mind minőségben kifogástalan, ezért bátran ajánljuk Vendégeink számára.</p>
+        <div><img src="http://cutlergyor.com/images/image/CutlerMegnyito/img_3639.jpg" width="250" height="50" alt="Kavezo"></img></div>
+        
+      </div>
+
+      
+        
+
+            <div><p>a</p></div>
+            <div className="flex flex-row justify-center flex-1">
+            <p className="w-1/2 mr-2 text-white items-center"><h1>2023.06.04 - Mobil töltési lehetőség</h1><br/>Ha lemerülőben a telefonod és az edzés ideje alatt biztonságos helyen szeretnéd feltölteni, akkor használd a mobiltöltő szekrényünket. Egyenként zárható és bármely típusú mobilodhoz megtalálod bennük a csatlakozást.</p>
+        <div><img src="https://img.alicdn.com/bao/uploaded/TB138z0h46I8KJjSszfXXaZVXXa.jpg" width="250" height="50" alt="Kavezo"></img></div>
+        
+      </div>
+      <div><p>a</p></div>
+      <div className="flex flex-row justify-center flex-1"></div>
+      
+        
+      </div>
+      
+
+        
     )
 }
