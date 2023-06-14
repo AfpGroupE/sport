7892434f5a37c8bacec14a50c720ed08613c92d7
vasadam1 <118050048+vasadam1@users.noreply.github.com>
2023-06-14 16:10:26 +0200

    Workout.js tartalommal kiegészítve
    Workout.js tartalommal kiegészítve

--- sport-oldal/client/src/pages/Workout.js
+++ sport-oldal/client/src/pages/Workout.js
@@ -3,7 +3,28 @@ import React from 'react'
 export default function Workout() {
     return (
         <div>
-            <h1>Edzéstervek</h1>
-        </div>
+        <h1 className="text-center font-mono font-extrabold m-5 text-5xl text-blue-100">Letölthető edzésterveink</h1>
+        
+        <div className="flex flex-row justify-center flex-1">
+        <table class="tg">
+
+    <tr>
+     <th class="tg-0lax" height= "10px"><div><img src="https://i.ytimg.com/vi/YwPPbZHPVA4/maxresdefault.jpg" width="250" height="50" alt="Kavezo"></img></div><p className="w-1/2 mr-2 text-white items-center"> <br></br>
+        <p>Kathi Béla & Bohos Kornél: Hogyan ösztönözzük magunkat?</p><br/>
+        <a href="https://www.linamar.hu/images/edzestervek.pdf"><u>Megnézem</u></a></p></th>
+
+     <th class="tg-0lax" height= "10px"><div><img src="https://i.ytimg.com/vi/YwPPbZHPVA4/maxresdefault.jpg" width="250" height="50" alt="Kavezo"></img></div><p className="w-1/2 mr-2 text-white items-center"> <br></br>
+     <p>Kathi Béla & Bohos Kornél: Testi és szellemi egészségünk</p><br/>
+        <a href="https://www.linamar.hu/images/edzestervek.pdf"><u>Megnézem</u></a></p></th>
+
+        
+          
+     </tr>
+    
+    
+    </table>
+    <p className="w-1/2 mr-2 text-white items-center">Ha a rendszeres testmozgás gyakorlása a célod, akkor a legjobb helyen jársz! Csak tölsd le bármelyik ingyenes edzésprogramunkat és használd azokat az online edzésnaplódban.<br/><br/>Szeretnél újítani meglévő edzésterveden vagy most kezdenél konditerembe járni? Fogyni, szálkásítani, tömeget növelni szeretnél vagy a teljesítményed szeretnéd fejleszteni, esetleg belekóstolnál egy új mozgásformába? Találd meg szakértőink által összeállított edzésterveink között a céljaidnak megfelelőt és lépj a következő szintre fitt életmódodban! Amiket biztosan megtalálsz nálunk: konditermi edzésterv kezdőknek és haladóknak; férfi és női edzéstervek; kardió és aerob edzéstervek; legjobb zsírégető gyakorlatok; edzésterv kezdő és haladó futóknak; HIIT edzésterv; otthoni edzés gyakorlatok.<br/><br/>Edzésterv nélkül nagyon nehezen fogsz célt érni. Szükség van arra, hogy követni tud a fejlődésedet. Amennyiben edzésterv nélkül, csak úgy "ösztönből" edzel, nagyon nehéz lesz ezt elérni. Persze, haladó szinten van aki képes a teste jelzéseire hallgatva dinamikusan változtatni az edzéseit, ez azonban évtizedes tapasztalat után reális csupán. Ebben az adatbázisban kezdő, haladó, és speciális edzéstervek közül egyaránt válogathatsz!<br/><br/> </p>
+    </div>
+    </div>
     )
 }
