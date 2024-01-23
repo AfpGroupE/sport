723257ab640c51be84f57e4b87e2d000829ee2ad
ocbtomi36 <118051723+ocbtomi36@users.noreply.github.com>
2024-01-23 15:49:42 +0100

    password has changed

--- sport-oldal/server/db.js
+++ sport-oldal/server/db.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const pool = new Pool({
     
     user: 'postgres',
-    password: 'Server',
+    password: 'passwort',
     host: "localhost",
     port: 5432,
     database: 'applikacio',
